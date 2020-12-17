70645e238f3b7d8a8ac2563993c1f67011cca0b7
Gijs Wobben <gijswobben@gmail.com>
2020-12-17 09:37:57 +0100

    Split the app file into routes, with a router

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,31 @@
 <script>
-	export let name;
+	import router from 'page';
+	import Home from './routes/Home.svelte';
+
+	let page;
+	let params;
+
+	// Define common middleware (used for every route)
+    const commonMiddleware = (ctx, next) => {
+        params = ctx.params;
+                next();
+        }
+
+    // Define the routes
+    router('*', commonMiddleware);
+	router('/', () => page = Home);
+
+	// Start the router
+	router.start();
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<svelte:component this={page} {params} />
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+	<div class="card-header">Header</div>
+	<div class="card-body">
+	  <h4 class="card-title">Primary card title</h4>
+	  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+	</div>
+</div>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let params;
+</script>
+
+<style lang="scss">
+	$title-color: #333;
+	h1 {
+		color: $title-color;
+	}
+</style>
+
+<main>
+	<h1>Hello world!</h1>
+	<p>
+		Visit the
+		<a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+		to learn how to build Svelte apps.
+	</p>
+</main>
