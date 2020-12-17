fb51851d85e38c4c1138810e64cfa8fc873f2cad
Gijs Wobben <gijswobben@gmail.com>
2020-12-17 09:37:41 +0100

    Import the global styles and removed unused props

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import App from './App.svelte';
 
+// Import global styles
+import './styles/global.scss';
+
 const app = new App({
 	target: document.body,
-	props: {
-		name: 'world'
-	}
+	props: {}
 });
 
 export default app;
